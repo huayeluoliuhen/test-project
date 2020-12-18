ddf737d4dac0565a6b3127cd4a4b60f4752ff02b
xutinglin <xutinglin@100yingcai.com>
2020-12-18 13:37:16 +0800

    1、修改目录结构。

---
+++ src/Example.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace terry\qiyuesuo;
+
+class Example
+{
+    public function show()
+    {
+        return  'hello world';
+    }
+
+    public function niu()
+    {
+        return '峰哥很牛';
+    }
+}
